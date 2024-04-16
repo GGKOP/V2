812d41895ebc20fcc66f4142dbfc6d19f4e60031
GGKOP <1097598746@qq.com>
2024-04-17 04:32:31 +0800

    fixed softmax

--- flash_attention.cu
+++ flash_attention.cu
@@ -12,50 +12,68 @@ void forward_kernel(const float* Q, const float* K, const float* V, const int N,
     // Offset into Q,K,V,O,l,m - different for each batch and head
     int q_offset = (by*gridDim.z*N*d + bz*N*d);
     //int kv_offset = (bz*gridDim.y*gridDim.x*N*d + by*gridDim.x*N*d );
-    int kv_offset = (by*gridDim.x*N*d + bz*N*d);  
-    int lm_offset = (bz*gridDim.y*gridDim.x*N+by*gridDim.x*N);
+    int kv_offset = (by*gridDim.z*N*d + bz*N*d);  
+    int lm_offset = (by*gridDim.y*N+by*N);
 ;
     //定义并分配sharing memory
     //其中Q要求分块加载但是必须要并行，所以要分配足够大的空间
     //由于K,V可以分块，所以给tile_size的空间足够
     //由于是并行所以S的空间也必须给够
     extern __shared__ float sram[];
-    int tile_size = Bc * d;
-    int tile_s_size =Bc * Br;  
+    int tile_q_size = Bc * d;
+    int tile_s_size = Bc * Br; 
+    int tile_kv_size =Br * d; 
     float* Qi = sram;
-    float* Kj = &sram[tile_size];
-    float* Vj = &sram[tile_size * 2 ];
-    float* S = &sram[tile_size * 2 + tile_s_size];
-    //float* O = &sram[tile_size * (Tc + ) + tile_s_size];
+    float* Kj = &sram[tile_q_size];
+    float* Vj = &sram[tile_q_size + tile_kv_size ];
+    float* Si = &sram[tile_q_size + (tile_kv_size *2)];
+    float* Oi = &sram[tile_q_size + (tile_kv_size *2) + tile_s_size];
         //采用Tr并行的方式加载Q
         for(int x=0;x<d;x++){
             //Qi[(tx * d) + x] = Q[qkv_offset + (tile_size * i) + (tx * d) + x];
-            Qi[(tx * d) + x] =Q[q_offset +tile_size * blockIdx.x + (tx * d) + x];
+            Qi[(tx * d) + x] =Q[q_offset +tile_q_size * blockIdx.x + (tx * d) + x];
         }
 
-        //load Kj Vj TO SRAM
+        for(int j=0;j<Tr;j++){
         float  row_m_prev = m[lm_offset+blockIdx.x*Bc+tx];
         float  row_l_prev = l[lm_offset+blockIdx.x*Bc+tx];
-        for(int j=0;j<Tr;j++){
-            //加载K,V到SRAM中
+        float  row_m = -INFNIITY;
+        //加载K,V到SRAM中,compute Si
         for(int m = 0;m<Br;m++ ){
-                        float sum =0;
+        float sum =0;
         for (int x = 0; x < d; x++) {
-            Kj[(tx * d) + x] = K[kv_offset + (tile_size * j) + (tx * d) + x];
-            Vj[(tx * d) + x] = V[kv_offset + (tile_size * j) + (tx * d) + x];
+            Kj[(tx * d) + x] = K[kv_offset + (tile_kv_size * j) + (tx * d) + x];
+            Vj[(tx * d) + x] = V[kv_offset + (tile_kv_size * j) + (tx * d) + x];
             //Vj[(tx * d) + x] = V[kv_offset + (tile_size * j) + (tx * d) + x];
             sum += Qi[(tx * d) + x] * Kj[(tx * d) + x];
         }
-       	    S[(tx * d) + m] = sum ;
+       	    Si[(tx * d) + m] = sum ;
+
+            // find row max
+            if(sum > row_m){
+                row_m =sum;
+            }
         }
 
-            //计算S的值
-            //计算O的值
-        for (int x = 0; x < d; x++) {
-            //Kj[(tx * d) + x] = K[kv_offset + (tile_size * j) + (tx * d) + x];
-            O[blockIdx.x*tile_size+(tx * d) + x]=S[blockIdx.x * Br * Bc + ( tx * Br) + x] * Vj[(tx * d) + x];
-        }       
+        float row_m_new = max(row_m_prev,row_m);
+        int sum_new = 0;
+
+        for(int x=0;x<Br;x++){
+            Si[(tx * d) + x] = __expf(Si[(tx * d) + x] - row_m_new);
+            sum_new += Si[(tx * d) + x] ;
+        }
+
+        float row_l_new = __expf(row_m_prev - row_m_new) * row_l_prev + sum_new ; 
+
+        for(int x = 0; x< Br; x++){
+            O[bx * tile_q_size +(tx *d)+x] =(__expf(row_m_prev - row_m_new) * O[(bx-1) * tile_q_size + (tx *d) + x ]+Si[(tx * d) +x] * Vj[(tx * d) + x])*(1 / row_l_new);
+        }
+        m[lm_offset + (Bc * blockIdx.x) + tx] = row_m_new;
+        l[lm_offset + (Bc * blockIdx.X) + tx ] = row_l_new;
+             
         } 
+
+
 
     }
 
